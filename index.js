c0dbc1b5c04f60e76e9935208e4b0b7c5a0b4858
rickie maweu <rickiemaweu@gmail.com>
2022-06-05 19:56:39 +0300

    commit tree c4319d9877c74f13c77fe8b5b2ec24e34bd7e490

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here!// Write your code here!
+// const body = document.body
+const main = document.getElementById('main')
+document.body.removeChild(main)
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+document.body.appendChild(newHeader)
